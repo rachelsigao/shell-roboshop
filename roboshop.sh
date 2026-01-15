b9191dfe3aa1af07160deaedef21ae2315f79ffe
rachelsigao <rachelsigao@gmail.com>
2026-01-14 19:19:36 -0500

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -8,5 +8,12 @@ DOMAIN_NAME="rachelsigao.online"
 
 for instance IN ${INSTANCES[@]}
 do
-    aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0c6ddf05e0664965e --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MyTestInstance}]'
+    INSTANCE_ID=$( aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0c6ddf05e0664965e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=MyTestInstance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then 
+    IP=aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text
+    else
+    IP=aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PublicIpAddress' --output text
+    fi
+    echo "$instance IP address: $IP
 done
