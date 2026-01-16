6b6a13e06ea1edd9ebaee27d08fd05e0e3b1ea16
rachelsigao <rachelsigao@gmail.com>
2026-01-16 13:40:08 -0500

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -9,7 +9,7 @@ DOMAIN_NAME="rachelsigao.online"
 #for instance in ${INSTANCES[@]}
 for instance in $@
 do
-    INSTANCE_ID=$( aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0c6ddf05e0664965e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0c6ddf05e0664965e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then 
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
