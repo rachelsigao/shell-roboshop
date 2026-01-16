91fd09568d4e13d8dfa046158273cf354eaa6037
rachelsigao <rachelsigao@gmail.com>
2026-01-15 20:18:30 -0500

    roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -45,7 +45,6 @@ VALIDATE $? "Installing Nodejs"
 
 #To avoid repeated executions
 id roboshop
-VALIDATE ()
     if [ $? -ne 0 ]
     then 
         useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
