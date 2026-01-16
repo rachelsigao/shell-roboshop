396809bc5a20133185b5ecb4822e633f918832da
rachelsigao <rachelsigao@gmail.com>
2026-01-16 16:10:16 -0500

    updated roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -9,30 +9,36 @@ DOMAIN_NAME="rachelsigao.online"
 #for instance in ${INSTANCES[@]}
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0c6ddf05e0664965e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
-    if [ $instance != "frontend" ]
+    INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id "$AMI_ID" \
+        --instance-type t3.micro \
+        --security-group-ids "$SG_ID" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+        --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ] 
     then 
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
-        RECORD_NAME="'$instance.$DOMAIN_NAME'"
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
-        RECORD_NAME="'$instance.$DOMAIN_NAME'"
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     fi
     echo "$instance IP address: $IP"
 
-    aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch '
-    {
-        "Comment": "Creating or Updating a record set for cognito endpoint"
-        ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'$RECORD_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$IP'"
-            }]
-        }
+     aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '{
+        "Comment": "Creating or Updating a record set for cognito endpoint",
+        "Changes": [{
+            "Action"              : "UPSERT",
+            "ResourceRecordSet"   : {
+                "Name"            : "$RECORD_NAME",
+                "Type"             : "A",
+                "TTL"              : 1,
+                "ResourceRecords"  : [{ 
+                    "Value"         : "$IP"
+                }]
+            }
         }]
     }'
 done
