942165c17b855947b103f130cda08f10146c832d
rachelsigao <rachelsigao@gmail.com>
2026-01-19 22:17:37 -0500

    updated roboshop

---
+++ cart.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs" 
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+#check for root access
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR: $N Please run this script with root access" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access, Script is being installed" | tee -a $LOG_FILE
+fi
+
+#Validate function takes input as exit status, what command they tried to install
+VALIDATE ()
+    {
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$2 is $G SUCCESS $N" | tee -a $LOG_FILE
+    else 
+        echo -e "$2 is $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+    }
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling Nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling Nodejs 20"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing Nodejs:20"
+
+#To avoid repeated executions
+id roboshop
+    if [ $? -ne 0 ]
+    then 
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+        VALIDATE $? "Creating roboshop system user" 
+    else 
+        echo -e "System user roboshop already created... $Y Skipping $N"
+    fi
+
+mkdir -p /app 
+VALIDATE $? "Creating app directory" 
+
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading cart"
+
+rm -rf /app/*
+cd /app 
+unzip /tmp/cart.zip &>>$LOG_FILE
+VALIDATE $? "Moving into app directory and unzipping cart"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "Installing dependencies"
+
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service 
+VALIDATE $? "Copying cart service"
+
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable cart &>>$LOG_FILE
+systemctl start cart
+VALIDATE $? "Reloading, enabling and starting cart server"  
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
